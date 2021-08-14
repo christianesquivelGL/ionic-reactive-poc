b781ad0a9c999359976cb2b8394dd69799d98767
christianesquivelGL <christian.esquivel@gorillalogic.com>
2021-08-13 21:22:33 -0600

    Adding more rxjs operators

--- src/app/features/observable-playground/observable-playground.page.ts
+++ src/app/features/observable-playground/observable-playground.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Parse from 'parse';
-import { from, Observable, of } from 'rxjs';
-import { first, map, mapTo, take } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
+import { filter, first, mapTo, take, takeLast } from 'rxjs/operators';
 import { AuthService } from 'src/app/providers/auth/auth.service';
 
 @Component({
@@ -10,36 +10,66 @@ import { AuthService } from 'src/app/providers/auth/auth.service';
   styleUrls: ['./observable-playground.page.scss'],
 })
 export class ObservablePlaygroundPage implements OnInit {
-  observable: Observable<any>;
-  usingFirst: Observable<any>;
-  usingTake: Observable<any>;
+  favorites$: Observable<any>;
+  planets$: Observable<any>;
+  usingFirst$: Observable<any>;
+  usingTake$: Observable<any>;
+  usingTakeLast$: Observable<any>;
+  usingFilter$: Observable<any>;
 
   constructor(private authService: AuthService) {}
 
   async ngOnInit() {
-    const raw = await this.fetch();
-    console.log('🚀 ~ raw data from parse', raw);
-    this.observable = from(raw);
+    const rawFavorites = await this.fetchFavorites();
+    console.log('🚀 ~ rawFavorites data from parse', rawFavorites);
+    this.favorites$ = from(rawFavorites);
 
-    this.usingFirst = this.observable.pipe(first());
-    console.log('🚀 ~ First array in observable');
-    this.usingFirst.pipe().subscribe((r) => console.log(r));
+    const rawPlanets = await this.fetchPlanetsFromSWAPI();
+    console.log('🚀 ~ rawPlanets data from SWAPI', rawPlanets);
+    this.planets$ = from(rawPlanets);
 
-    this.usingTake = this.observable.pipe(take(3));
+    this.usingFirst$ = this.favorites$.pipe(first());
+    console.log('🚀 ~ first() from observable');
+    this.usingFirst$.subscribe((r) => console.log(r));
+
+    this.usingTake$ = this.favorites$.pipe(take(3));
     console.log('🚀 ~ take(3) from observable');
-    this.usingTake.pipe().subscribe((r) => console.log(r));
+    this.usingTake$.subscribe((r) => console.log(r));
 
-    const greetings = this.observable.pipe(
-      mapTo('🚀 ~ Anonnymous fn called using rxjs.mapTo()'),
+    this.usingTakeLast$ = this.favorites$.pipe(takeLast(2));
+    console.log('🚀 ~ takeLast(2) from observable');
+    this.usingTakeLast$.subscribe((r) => console.log(r));
+
+    const usingMapTo = this.favorites$.pipe(
+      mapTo('🚀 ~ Anonnymous fn called using mapTo()'),
     );
-    greetings.subscribe((x) => console.log(x));
+    usingMapTo.subscribe((x) => console.log(x));
+
+    this.usingFilter$ = this.planets$.pipe(
+      filter((f) => f.get('name') === 'Alderaan'),
+    );
+    this.usingFilter$.subscribe((r) => {
+      console.log(
+        '🚀 ~ filter() from observable where name equals Alderaan -> ',
+        r,
+      );
+    });
   }
 
-  fetch() {
+  fetchFavorites() {
     const obj = Parse.Object.extend('Favorites');
     const query = new Parse.Query(obj);
     query.equalTo('user', this.authService.getCurrentUser());
     query.include('SWAPI_Character');
+
+    return query.find();
+  }
+
+  fetchPlanetsFromSWAPI() {
+    const obj = Parse.Object.extend('SWAPI_Planet');
+    const query = new Parse.Query(obj);
+    query.ascending('name');
+    query.limit(10);
 
     return query.find();
   }
