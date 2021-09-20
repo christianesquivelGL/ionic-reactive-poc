8156e4250cdbb8bcb56cf9f5fc8ef10bac203952
Christian Esquivel <christian.esquivel@gorillalogic.com>
2021-09-20 15:23:03 -0600

    Logging for server, root planets basic list from cloud process

--- server/cloud/main.js
+++ server/cloud/main.js
@@ -1,8 +1,13 @@
+const _ = require('lodash');
+
 const Planet = Parse.Object.extend('Planet');
 
 Parse.Cloud.define('v1_getPlanets', async (req) => {
   const query = new Parse.Query(Planet);
   query.ascending('name');
+
+  console.log(req);
+  console.log(_.VERSION);
 
   return query.find();
 });
